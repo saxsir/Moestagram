433af86681a2237a438fc48760f955d6dffb946d
saxsir <saxsir.256@gmail.com>
2015-02-21 16:23:41 +0900

    [clean] refs #5 ソースにコメントをつける

--- Moestagram/ViewController.swift
+++ Moestagram/ViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    /*
+     * Main.storyboardとの接続周りはこちら
+     */
     @IBOutlet weak var launchCameraButton: UIBarButtonItem!
 
+
+    // カメラボタンが押された時に呼ばれる（ようにMain.storyboardで設定した）
     @IBAction func launchCameraButtonTapped(sender: UIBarButtonItem) {
         let camera = UIImagePickerControllerSourceType.Camera
 
         if (UIImagePickerController.isSourceTypeAvailable(camera)) {
             var picker = UIImagePickerController()
             picker.sourceType = camera
-            picker.delegate = self
+            // refs http://qiita.com/zucay/items/03080645dde1a62ec570
+            picker.delegate = self //pickerさん、何かあったら私（ViewController）を呼んでね
             picker.allowsEditing = false
             self.presentViewController(picker, animated: true, completion: nil)
         } else {
@@ -28,13 +34,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.presentViewController(alert, animated: true, completion: nil)
         }
     }
-    
+
+    // imagePickerControllerさんが写真が撮影されたら呼んでくれる（そういう決まり）
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         let image = info[UIImagePickerControllerOriginalImage] as UIImage
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
+    // アプリが起動して画面がロードされたら呼ばれる
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
