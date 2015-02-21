b4a2aefe6f26b7f603c708ac72d93ca528f308e1
saxsir <saxsir.256@gmail.com>
2015-02-21 17:29:36 +0900

    [wip] refs #6 CollectionViewCellのレイアウトを扱うクラスを作成

---
+++ Moestagram/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  Moestagram
+//
+//  Created by Shohei Sasamoto on 2015/02/21.
+//  Copyright (c) 2015年 saxsir. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+}
