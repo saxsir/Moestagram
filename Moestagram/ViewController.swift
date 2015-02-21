fc0cec38032cf5d81740979b3f98df938956ef21
saxsir <saxsir.256@gmail.com>
2015-02-21 14:08:36 +0900

    Initial Commit

---
+++ Moestagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Moestagram
+//
+//  Created by Shohei Sasamoto on 2015/02/21.
+//  Copyright (c) 2015年 saxsir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
