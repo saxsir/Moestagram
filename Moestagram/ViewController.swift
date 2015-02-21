c7a6778c0e68487f62d27543c6706b81b252fb5d
saxsir <saxsir.256@gmail.com>
2015-02-21 14:56:40 +0900

    [add] refs #2 footerにナビゲーションバー（タブバー？）を追加
    カメラボタンをタップした時のアクションを接続して終了

--- Moestagram/ViewController.swift
+++ Moestagram/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var launchCameraButton: UIBarButtonItem!
 
+    @IBAction func launchCameraButtonTapped(sender: UIBarButtonItem) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
