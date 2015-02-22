c1b58f36a4638e2c2b44bb9ff84188d958392e26
saxsir <saxsir.256@gmail.com>
2015-02-22 20:40:29 +0900

    [wip] refs #16 新しいViewControllerを管理するクラスを作成、storyboardで接続

---
+++ Moestagram/ImageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ImageViewController.swift
+//  Moestagram
+//
+//  Created by Shohei Sasamoto on 2015/02/22.
+//  Copyright (c) 2015年 saxsir. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
