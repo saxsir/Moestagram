acc29abb044691ec47b521e819b11566c6198f82
saxsir <saxsir.256@gmail.com>
2015-03-07 17:31:15 +0900

    [wip] アプリ初回起動時に通知の許可を求めるダイアログを表示するようになった

--- Moestagram/AppDelegate.swift
+++ Moestagram/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // ローカル通知の許可をください
+        let settings = UIUserNotificationSettings(
+            forTypes: UIUserNotificationType.Badge
+                | UIUserNotificationType.Sound
+                | UIUserNotificationType.Alert,
+            categories: nil)
+        application.registerUserNotificationSettings(settings)
+        
         return true
     }
 
@@ -41,6 +50,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
