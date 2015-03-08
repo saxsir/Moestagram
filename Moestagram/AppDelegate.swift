520b5d94d1528ae052c46af54d3bc634f7715526
saxsir <saxsir.256@gmail.com>
2015-03-08 12:34:46 +0900

    [add] refs #17 毎日ランダムな時間にPUSH通知を送りたい
    定型文を毎日ランダムな時間に送るようにした

--- Moestagram/AppDelegate.swift
+++ Moestagram/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    // アプリ起動時
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -37,7 +36,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 
-        setNotification(10)
+        // 登録されている通知を全てキャンセル
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+
+        // 新しくPush通知（ローカル）を登録する - 1週間分、毎日ランダムな時間に。
+        scheduleDailyNotificationForOneWeek()
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
@@ -51,8 +54,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-    
-    
+
     /**
      * 通知周りの処理
      *
@@ -66,21 +68,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         alert.addButtonWithTitle(notification.alertAction!)
         alert.show()
     }
-    
+
     /**
      * プライベートメソッド
      */
-    // Push通知をセットする
-    func setNotification(interval: NSTimeInterval) {
-        // 登録されている通知を削除する
-        UIApplication.sharedApplication().cancelAllLocalNotifications()
-        
+    // 1週間分のデイリー通知を登録する関数
+    func scheduleDailyNotificationForOneWeek() {
+        for i in 1...7 {
+            let fireDate = generateRandomTimeInXDays(i)
+            let alertBody = "通知本文"
+            addLocalNotification(fireDate, alertBody: alertBody)
+        }
+    }
+
+    // x日後のランダムな時刻の日付オブジェクトを返す関数
+    func generateRandomTimeInXDays(x: Int) -> NSDate {
+        let hours = [8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23] //日本時間
+        let rand = Int(arc4random_uniform(UInt32(hours.count)))
+
+        // 24 * x時間後の時刻を取得
+        let tomorrow = NSDate(timeIntervalSinceNow: NSTimeInterval(3600 * 24 * x))
+
+        // 時刻だけ更新
+        let calendar = NSCalendar(identifier: NSGregorianCalendar)!
+        let date = calendar.dateBySettingHour(hours[rand], minute: 00, second: 0, ofDate: tomorrow, options: nil)!
+
+        //XXX: 日本時間とGMT時間考慮してないけどなぜかよしなに設定されている...？
+        return date //GMT標準時間
+    }
+
+    // ローカル通知を登録する関数
+    func addLocalNotification(fireDate: NSDate, alertBody: String) {
         var notification = UILocalNotification()
-        notification.fireDate = NSDate(timeIntervalSinceNow: interval)
-        notification.alertBody = "message"
+        notification.fireDate = fireDate
+        notification.alertBody = alertBody
         notification.alertAction = "OK"
         notification.soundName = UILocalNotificationDefaultSoundName
         UIApplication.sharedApplication().scheduleLocalNotification(notification)
     }
-
 }
