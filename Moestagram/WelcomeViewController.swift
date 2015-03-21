0f4c7e0b44d8fb915644d6dfad4120fe16963151
saxsir <saxsir.256@gmail.com>
2015-03-21 13:16:03 +0900

    [wip] refs #25 add WelcomeViewController

---
+++ Moestagram/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewController.swift
+//  Moestagram
+//
+//  Created by Shohei Sasamoto on 2015/03/21.
+//  Copyright (c) 2015年 saxsir. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import Photos
+
+class WelcomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
