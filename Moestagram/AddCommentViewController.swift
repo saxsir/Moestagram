b93f10a30c2306e761e4cce94d678d2d8a64fe11
saxsir <saxsir.256+github@gmail.com>
2015-04-18 01:27:42 +0900

    [add] Add new view controller, AddCommentViewController
    - 撮った写真にコメントを追加する画面
    - 写真撮影後に自動で表示される予定

---
+++ Moestagram/AddCommentViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddCommentViewController.swift
+//  Moestagram
+//
+//  Created by Shohei Sasamoto on 2015/04/18.
+//  Copyright (c) 2015年 saxsir. All rights reserved.
+//
+
+import UIKit
+
+class AddCommentViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
