29cf44640fa9d25c4c7e328fbc8b99ee68715622
saxsir <saxsir.256+github@gmail.com>
2015-04-18 11:48:56 +0900

    [add] AddCommentViewで最後に撮影した写真を表示
    - segueで変数渡すのではなく, 最後に撮影した画像を取得している

--- Moestagram/AddCommentViewController.swift
+++ Moestagram/AddCommentViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Photos
 
 class AddCommentViewController: UIViewController {
     
-    
+    @IBOutlet weak var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // 最後に撮影した画像を取得
+        var options = PHFetchOptions()
+        options.sortDescriptors = [
+            NSSortDescriptor(key: "creationDate", ascending: false)
+        ]
+        let asset = PHAsset.fetchAssetsWithMediaType(.Image, options: options).firstObject as! PHAsset
+
+        let manager: PHImageManager = PHImageManager()
+        manager.requestImageForAsset(
+            asset,
+            targetSize: imageView.frame.size,
+            contentMode: .AspectFit,
+            options: nil) { (image, info) -> Void in
+                self.imageView.contentMode = .ScaleAspectFit
+                self.imageView.image = image
+        }
     }
     
     override func didReceiveMemoryWarning() {
