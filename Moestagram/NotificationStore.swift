13a27297108d96f42e841b45505b3b2cfb177a53
saxsir <saxsir.256+github@gmail.com>
2015-04-18 17:35:20 +0900

    [add] refs #36 Add new model, NotificationStore - [fix] NotificationStoreモデル -> NotificationStoreクラスの対応づけ

---
+++ Moestagram/NotificationStore.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationStore.swift
+//  Moestagram
+//
+//  Created by Shohei Sasamoto on 2015/04/18.
+//  Copyright (c) 2015年 saxsir. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(NotificationStore)
+class NotificationStore: NSManagedObject {
+
+    @NSManaged var title: String
+    @NSManaged var body: String
+    @NSManaged var fire_date: NSDate
+    @NSManaged var is_checked: Bool
+
+}
