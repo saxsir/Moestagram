bb8683279b96d1fe68754332e5f59db569ab3e3f
saxsir <saxsir.256+github@gmail.com>
2015-05-04 14:37:54 +0900

    サンプル画像を追加 refs #67
    - 画像が増えたのでResourcesフォルダを作成
    - ついでに、いつの間にかcommitされてたコントローラーを削除

--- Moestagram/CustomAlertViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CustomAlertViewController.swift
-//  Moestagram
-//
-//  Created by Shohei Sasamoto on 2015/05/03.
-//  Copyright (c) 2015年 saxsir. All rights reserved.
-//
-
-import Foundation
